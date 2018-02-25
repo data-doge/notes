e933cbff63a0a634d972d3a3ad78b18597897711
Eugene Lynch <eugene@enspiral.com>
2018-02-25 13:14:31 -0500

    new logs

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-
 # notes
 
 notes
+
+## entry
+
+- timestamp
+- create file with `${timestamp}.md` in `entries`
+- index.js reads from entries, ordering by timestamp, inverse, streams onto screen
--- entries.js
+++ entries.js
@@ -1,3 +1,4 @@
 module.exports = [
-  "<p>trust by default</p>"
+  "<p>trust by default</p>",
+  "<p><img src=\"https://earthlingsinternational.files.wordpress.com/2014/07/family-in-pool.jpg?w=700&h=\" alt=\"the latest chicken scandal\" /></p>"
 ]
