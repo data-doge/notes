7de160eca0beea4d57ba5e9f507f79b88fc0ec93
Eugene Lynch <eugene@enspiral.com>
2018-02-25 13:41:40 -0500

    new logs

--- entries.js
+++ entries.js
@@ -1,5 +1,9 @@
 module.exports = [
   {
+    "ts": "2018-02-25T13:39:55-05:00",
+    "html": "<p>pigeons used to be royalty, but actually they're still royalty. lots of people look at pigeons and think that they are trash birds but then people also feed them bread for fun which sounds a lot like royalty so it looks like pigeons are on top once again.</p>"
+  },
+  {
     "ts": "2018-02-25T13:30:57-05:00",
     "html": "<p><a href=\"https://www.google.com/search?q=weather&oq=weather&aqs=chrome.0.0l6.702j0j1&sourceid=chrome&ie=UTF-8\">here is the weather is</a></p>"
   },
